4857af779b7919800d4d4e49d67d097d0d95c876
Michael Kaufman <michael.kaufman@syngenta.com>
2021-03-26 09:47:23 -0500

    LDBAPP-2522: Getting request header auth to work (#3)
    * feat: Getting request header auth to work
    
    * feat: Use env version until cert issue resolved

--- src/organizations/organizations.service.ts
+++ src/organizations/organizations.service.ts
@@ -1,4 +1,4 @@
-import { HttpService, Injectable } from '@nestjs/common';
+import { HttpService, Injectable, Request } from '@nestjs/common';
 import { CreateOrganizationDto } from './dto/create-organization.dto';
 import { UpdateOrganizationDto } from './dto/update-organization.dto';
 
@@ -7,12 +7,27 @@ import { map } from 'rxjs/operators';
 @Injectable()
 export class OrganizationsService {
   constructor(private httpService: HttpService) {}
+  private readonly request: Request;
 
   create(createOrganizationDto: CreateOrganizationDto) {
     return 'This action adds a new organization';
   }
 
-  findAll() {
+  // findAll(request: Request) {
+  //   const headers = request.headers;
+
+  //   console.log(headers);
+
+  //   return this.httpService
+  //     .get('https://api.staging.base.cropwise.com/v2/orgs', {
+  //       headers,
+  //     })
+  //     .pipe(map((response) => response.data));
+  // }
+
+  // Below is implementation using env variable
+
+  findAll(request: Request) {
     const bearerToken = process.env.BEARER_TOKEN;
 
     return this.httpService
